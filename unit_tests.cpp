3d1a23c2fd0ff9a569f8fc23d62a0263f1978c85
Sohail Grewal <sgrewal118@my.bcit.ca>
2024-09-18 13:04:10 -0700

    added basic test case to unit_tests.cpp

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -1,3 +1,12 @@
 //
 // Created by Sohail on 2024-09-18.
 //
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "myStack.hpp"
+TEST_CASE("A new stack is empty", "testTag1") {
+    MyStack tester;
+
+    REQUIRE(tester.isEmpty() == true);
+    REQUIRE(tester.isFull() == false);
+}
